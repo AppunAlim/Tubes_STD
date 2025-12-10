e63bd47e8a83cfc407b6e5b1e1ed0bc666e1fcf3
AppunAlim <affansalim06@gmail.com>
2025-12-10 13:01:19 +0700

    update header + source dikit

--- header.h
+++ header.h
@@ -2,6 +2,28 @@
 #define HEADER_H_INCLUDED
 #include <iostream>
 
+using namespace std;
+
+typedef struct Node* adrNode;
+
+struct Infotype {
+    string nama;
+    string tipe;
+};
+
+struct Node {
+    Infotype info;
+    adrNode firstChild;
+    adrNode nextSibling;
+};
+
+adrNode newNode(string nama, string tipe);
+void insertNode(adrNode parent, string nama, string tipe);
+void printTree(adrNode root, int level);
+void preOrder(adrNode root);
+void postOrder(adrNode root);
+void inOrder(adrNode root);
+adrNode findNode(adrNode root, string nama);
 
 
 #endif // HEADER_H_INCLUDED
--- source.cpp
+++ source.cpp
@@ -1,2 +1,46 @@
 #include "header.h"
 #include <iostream>
+
+using namespace std;
+
+
+adrNode newNode(string nama, string tipe) {
+    adrNode p = new Node;
+    p->info.nama = nama;
+    p->info.tipe = tipe;
+    p->firstChild = nullptr;
+    p->nextSibling = nullptr;
+    return p;
+}
+
+void insertNode(adrNode parent, string nama, string tipe) {
+    if (parent == nullptr){
+        return nullptr;
+    }
+
+    adrNode pBaru = newNode(nama, tipe);
+
+    if (parent->firstChild == nullptr) {
+        parent->firstChild = pBaru;
+    } else {
+        adrNode temp = parent->firstChild;
+        while (temp->nextSibling != nullptr) {
+            temp = temp->nextSibling;
+        }
+        temp->nextSibling = pBaru;
+    }
+}
+
+adrNode findNode(adrNode root, string nama) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+    if (root->info.nama == nama){
+        return root
+    }
+    adrNode hasil = findNode(root->firstChild, nama);
+    if (hasil != nullptr) {
+        return hasil;
+    }
+    return findNode(root->nextSibling, nama);
+}
